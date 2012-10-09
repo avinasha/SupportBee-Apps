cfb05d2f138034cb71f9b5c6ec9653b78f3f366f
Avinasha Shastry <me@avinasha.com>
2012-10-09 11:28:18 +0700

    Missing file

---
+++ lib/supportbee/ticket.rb
@@ -0,0 +1,31 @@
+module SupportBee
+  class Ticket < Resource
+    class << self
+      def list(auth={},params={})
+        response = api_get(url,auth,params)
+        tickets = []
+        result = Hashie::Mash.new
+        response.body.keys.each do |key|
+          if key == 'tickets'
+            response.body[key].each do |ticket|
+              tickets << self.new(auth,ticket)
+            end
+          else
+            result[key] = response.body[key]
+          end
+        end
+        result.tickets = tickets
+        result
+      end
+
+      def create(auth={},params={})
+        response = api_post(url,auth,params)
+        self.new(auth,response.body['ticket'])
+      end
+    end
+
+    def delete
+      raise NotImplementedError.new('A Ticket cannot be deleted')
+    end
+  end
+end
